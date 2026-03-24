614f898bc8f063459d969de17ca0b0c8d934b10a
Rais Ahmad <raismansuri74059@gmail.com>
2026-03-24 17:20:34 +0530

    lkk

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'vyapar-pos-be.vercel.app/api',
+  apiUrl: 'https://vyapar-pos-be-git-development-raismansuri74059-1745s-projects.vercel.app/api',
   aiModelId: 'llama-3.1-8b-instant'//'google/gemini-2.5-flash'
 };
