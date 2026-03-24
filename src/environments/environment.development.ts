b72b9438d3d3325d35f6610340bf97cd300b11c2
Rais Ahmad <raismansuri74059@gmail.com>
2026-03-24 17:08:13 +0530

    czxc

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'vyapar-pos-be.vercel.app',
+  apiUrl: 'vyapar-pos-be.vercel.app/api',
   aiModelId: 'llama-3.1-8b-instant'//'google/gemini-2.5-flash'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'vyapar-pos-be.vercel.app',
+  apiUrl: 'vyapar-pos-be.vercel.app/api',
   aiModelId: 'llama-3.1-8b-instant'//'google/gemini-2.5-flash'
 };
