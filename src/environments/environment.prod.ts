2571ca20875a86910611dfdd0a4276462d2dc70e
Rais Ahmad <raismansuri74059@gmail.com>
2026-03-25 14:42:46 +0530

    prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  local: false,
+  production: true,
+  apiUrl: 'https://vyapar-pos-be-git-development-raismansuri74059-1745s-projects.vercel.app/api',
+  aiModelId: 'llama-3.1-8b-instant'
+};
