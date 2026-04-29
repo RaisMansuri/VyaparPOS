22c2a40a6a6450696b24e6ff93c96944ff00980d
RaisMansuri <raismansuri74059@gmail.com>
2026-04-29 18:46:02 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   local: true,
   production: false,
-  apiUrl: 'https://vyapar-pos-be-git-development-raismansuri74059-1745s-projects.vercel.app/api',
+  apiUrl: 'https://vyapar-pos-be.vercel.app/api',
   aiModelId: 'llama-3.1-8b-instant'
 };
